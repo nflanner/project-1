ef1da8c9cde489e7ddcf7fab40282b188d5adc5d
Nourizadeh29 <101478716+Nourizadeh29@users.noreply.github.com>
2022-05-19 21:24:33 -0700

    finished logic (#11)

--- Assets/js/index-script.js
+++ Assets/js/index-script.js
@@ -8,6 +8,35 @@ var movieImgEl = document.querySelector('#img-el');
 
 var genres = ['Action', 'Comedy', 'Drama', 'Family', 'Horror', 'Romance', 'Science-Fiction'];
 var selectionTitle = '';
+var genreMap ={
+    28: 'Action',
+    12: 'Adventure',
+    16: 'Animation',
+    35: 'Comedy',
+    80: 'Crime',
+    99: 'Documentary',
+    18: 'Drama',
+    10751: 'Family',
+    14: 'Fantasy',
+    36: 'History',
+    27: 'Horror',
+    10402: 'Music',
+    9648: 'Mystery',
+    10749: 'Romance',
+    878: 'Science Fiction',
+    10770: 'TV Movie',
+    53: 'Thriller',
+    10752: 'War',
+    37: 'Western'
+}
+var genreList=[];
+var titleList=[];
+var scoreList=[]; 
+var saveObject={
+    genre: [],
+    title: [],
+    score: [],
+}
 // 0: {id: 28, name: 'Action'}
 // 1: {id: 12, name: 'Adventure'}
 // 2: {id: 16, name: 'Animation'}
@@ -28,6 +57,7 @@ var selectionTitle = '';
 // 17: {id: 10752, name: 'War'}
 // 18: {id: 37, name: 'Western'}
 
+
 // start the roulette
 function handleStart(event) {
     event.preventDefault();
@@ -38,7 +68,6 @@ function handleStart(event) {
     if (genreInput == 'Genre') {
         genreInput = 35;
     }
-    console.log(genreInput);
     getMovieInfo(genreInput);
 }
 
@@ -68,12 +97,8 @@ function getMovieInfo(genre) {
     fetch('https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=' + genre + '&page=' + pageNum, options)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             var movieSelection = data.results[movieSelectionNum];
             selectionTitle = movieSelection.title;
-            console.log(selectionTitle);
-            console.log(movieSelection);
-
             hydrate(movieSelection);
         })
         .catch(err => console.error(err));
@@ -91,3 +116,74 @@ function hydrate(movie) {
 
 startRouletteEl.addEventListener("click", handleStart);
 additionalEl.addEventListener("click", handleAdditional);
+
+  ////////////////////////////////////
+  var movieTitleEl = document.querySelector('#title');
+  var movieScoreEl = document.querySelector('#score');
+  var movieGenreEl = document.querySelector('#genre-bottom');
+  var savedBttnEl = document.querySelector('#save');
+  var listEl=document.querySelector('#saved-list')
+
+  savedBttnEl.addEventListener("click", handleSave);
+
+function handleSave(event){
+    event.preventDefault();
+    var movieTitle = movieTitleEl.value; 
+    var movieScore = movieScoreEl.value;
+    var movieGenre = genreMap[movieGenreEl.value];
+
+    titleList.push(movieTitle);
+    scoreList.push(movieScore);
+    genreList.push(movieGenre);
+    saveToLocal();
+    appendList(movieTitle, movieScore, movieGenre);
+}
+
+function saveToLocal(){
+
+    for(var i=0;i<titleList.length;i++){
+
+        if(!saveObject["title"].includes(titleList[i])){
+            saveObject["title"].push(titleList[i])
+            saveObject["score"].push(scoreList[i])
+            saveObject["genre"].push(genreList[i])
+
+        }
+    
+    }
+
+    var saveJSON=JSON.stringify(saveObject)
+    localStorage.setItem("saveObject",saveJSON)
+} 
+
+function pullStorage(){
+
+    var saveString=localStorage.getItem("saveObject")
+    var parsedSave=JSON.parse(saveString)
+    for(var i=0;i<parsedSave["title"].length;i++){
+        titleList.push(parsedSave["title"][i])
+        scoreList.push(parsedSave["score"][i])
+        genreList.push(parsedSave["genre"][i])
+
+    }
+}
+
+function appendList(movieTitle, movieScore, movieGenre){
+    var listData=document.createElement('li')
+    listData.setAttribute('class',"list-group-item text-dark")
+    
+    var listString='Title: '+movieTitle+', Score: '+movieScore+', Genre: '+movieGenre
+    listData.textContent=listString
+    listEl.appendChild(listData)
+
+}
+
+function populateList(){
+
+    for(var i=0;i<titleList.length;i++){
+        appendList(titleList[i],scoreList[i],genreList[i])
+    }
+}
+
+pullStorage();
+populateList()
